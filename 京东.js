407486c6c61cd611d459249dc53b8e6be20e0303
北慕白 <63459931+w1770946466@users.noreply.github.com>
2021-11-15 12:17:08 +0800

    Add files via upload

---
+++ 京东.js
@@ -0,0 +1,336 @@
+[ {
+    "id": "jd_fanli",
+	"cron": "0 40 0,2 * * * *",
+	"describe":"活动入口：https://ifanli.m.jd.com/rebate/earnBean.html?paltform=null",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_fanli.js",
+	"name": "饭粒"
+},{
+	"id": "jd_lotteryMachine_xcl",
+	"cron": "0 1 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/script/jd_lotteryMachine.js",
+	"name": "京东抽奖机"
+}, {
+	"id": "jd_DailyBonus_Mod",
+	"cron": "0 2 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_bean_sign.js",
+	"name": "多合一签到"
+}, {
+	"id": "jd_GoldcoinToGift",
+	"cron": "0 4 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_GoldcoinToGift.js",
+	"name": "攒金币 赢大礼"
+}, {
+	"id": "jd_jdfactory_lxk0301",
+	"cron": "0 5 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_jdfactory.js",
+	"name": "东东工厂"
+}, {
+	"id": "jd_dtqd",
+	"cron": "0 6 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/script/jd_dtqd.js",
+	"name": "店铺签到"
+}, {
+	"id": "jd_club_lottery",
+	"cron": "0 7 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/zero205/JD_tencent_scf/main/jd_club_lottery.js",
+	"name": "摇京豆"
+}, {
+	"id": "jd_superMarket_xcl",
+	"cron": "0 8 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_superMarket.js",
+	"name": "京东超市"
+}, {
+	"id": "jd_blueCoin",
+	"cron": "55 59 23 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/w1770946466/JD-is-javascrip/main/jd_blueCoin.js",
+	"name": "京小超兑换奖品"
+}, {
+	"id": "jd_live_redrain",
+	"cron": "0 0,1,30,31 20-23/1 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_live.js",
+	"name": "超级直播间红包雨"
+}, {
+	"id": "jx_nc_xcl",
+	"cron": "0 0 9,12,18 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_jxnc.js",
+	"name": "京喜农场"
+}, {
+	"id": "jd_joy_xcl",
+	"cron": "0 15 0-23/2 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_joy.js",
+	"name": "宠汪汪"
+}, {
+	"id": "jd_pet_xcl",
+	"cron": "0 15 6-18/6 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_pet.js",
+	"name": "东东萌宠"
+}, {
+	"id": "jd_dreamFactory_xcl",
+	"cron": "0 10 * * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_dreamFactory.js",
+	"name": "京喜工厂"
+}, {
+	"id": "jd_moneyTree_xcl",
+	"cron": "0 3 * * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_moneyTree.js",
+	"name": "京东摇钱树"
+}, {
+	"id": "jd_nzmh_xcl",
+	"cron": "0 0 8 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_nzmh.js",
+	"name": "女装盲盒"
+}, {
+	"id": "jd_speed_red_xcl",
+	"cron": "0 0 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_speed_redpocet.js",
+	"name": "极速版领红包"
+}, {
+	"id": "jd_shop_xcl",
+	"cron": "0 10 0 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_shop.js",
+	"name": "进店领豆"
+}, {
+	"id": "jd_ms_xcl",
+	"cron": "0 10 7 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_ms.js",
+	"name": "京东秒秒币"
+}, {
+	"id": "jd_speed_sign_xcl",
+	"cron": "0 0 7 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_speed_signfaker.js",
+	"name": "极速版签到赚金币"
+},{
+	"id": "jd_cash",
+	"cron": "55 59 23 * * * *",
+	"describe":" 签到领现金，每日2毛～5毛可互助，助力码每日不变，只变日期，活动入口：京东APP搜索领现金进入",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_cash.js",
+	"name": "签到领现金"
+},{
+	"id": "jd_jdzz_xcl",
+	"cron": "0 10 0 * * * *",
+	"describe":"长期活动，每日收益2毛左右，多号互助会较多",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_jdzz.js",
+	"name": "京东赚赚"
+},{
+	"id": "jx_sign_xcl",
+	"cron": "0 5 0 * * * *",
+	"describe":"京喜签到",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jx_sign.js",
+	"name": "京喜签到"
+},{
+	"id": "jx_cfd_xcl",
+	"cron": "0 5 0,8,13,19 * * * *",
+	"describe":"京喜APP-我的-京喜财富岛",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jx_cfd.js",
+	"name": "京喜财富岛"
+},{
+	"id": "jd_speed_signfree",
+	"cron": "0 18 8,12,20 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfd_loop.js",
+	"name": "财富岛热气球"
+},{
+	"id": "jd_speed_signfree",
+	"cron": "0 18 8,12,20 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfd_mooncake.js",
+	"name": "财富岛合月饼"
+},{
+	"id": "z_mother_jump_xcl",
+	"cron": "0 5 8,14,20 6-13 4 * *",
+	"describe":"活动入口: 首页-母婴馆-跳一跳",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/z_mother_jump.js",
+	"name": "母婴跳一跳"
+},{
+	"id": "jd_fruit_xcl",
+	"cron": "0 5 6-18/6 * * * *",
+	"describe":"活动入口：京东APP-我的-东东农场",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_fruit.js",
+	"name": "东东农场"
+},{
+	"id": "jd_rankingList_xcl",
+	"cron": "0 11 9 * * * *",
+	"describe":"活动入口：京东APP首页-更多频道-排行榜-悬浮按钮",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_rankingList.js",
+	"name": "京东排行榜"
+},
+{
+	"id": "jd_unsubscribe_xcl",
+	"cron": "0 0 8 * * * *",
+	"describe":"店铺批量取消关注，建议定时在试用之前",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_unsubscribe.js",
+	"name": "店铺取消关注"
+},{
+	"id": "jd_health_area_xcl",
+	"cron": "0 10 0-23/4 * * * *",
+	"describe":"京东APP首页搜索 \"玩一玩\"即可",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_health.js",
+	"name": "东东健康社区"
+},{
+	"id": "jd_market_lottery_xcl",
+	"cron": "0 4 10 * * * *",
+	"describe":"幸运大转盘 活动地址：https://pro.m.jd.com/mall/active/3ryu78eKuLyY5YipWWVSeRQEpLQP/index.html",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_market_lottery.js",
+	"name": "幸运大转盘"
+},{
+	"id": "jd_super_redrain_xcl",
+	"cron": "0 1 0-23/1 * * * *",
+	"describe":"整点京豆雨",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_super_redrain.js",
+	"name": "整点京豆雨"
+},{
+	"id": "jd_jin_tie_xcl",
+	"cron": "0 10 0 * * * *",
+	"describe":"领金贴(只做签到以及互助任务里面的部分任务)",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_jin_tie_xh.js",
+	"name": "领金贴"
+},{
+	"id": "jd_half_rain",
+	"cron": "0 0,1,30,31 16-23/1 * * * *",
+	"isConcurrent":true,
+	"describe":"京东半点红包雨",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_half_redrain.js",
+	"name": "半点红包雨"
+},{
+	"id": "jd_bean_home",
+	"cron": "0 10 7 * * * *",
+	"isConcurrent":false,
+	"describe":"活动入口：京东APP首页-领京豆",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_bean_home.js",
+	"name": "领京豆额外奖励"
+},{
+	"id": "jd_gold_creator",
+	"isConcurrent":true,
+	"cron": "0 13 1,22 * * * *",
+	"describe":"活动入口：https://h5.m.jd.com/babelDiy/Zeus/2H5Ng86mUJLXToEo57qWkJkjFPxw/index.html",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_gold_creator.js",
+	"name": "金榜创造营"
+},{
+	"id": "jd_jsffl",
+	"isConcurrent":true,
+	  "cron": "0 0 * * * * *",
+	"describe":"京东极速版-我的-省钱大赢家-翻翻乐",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_jsffl.js",
+	"name": "极速版翻翻乐"
+},{
+	"id": "jx_mc",
+	"isConcurrent":true,
+	  "cron": "0 0 * * * * *",
+	"describe":"京喜App -> 我的 -> 京喜牧场，先将新手任务做完，再执行本脚本，不然会出现未知错误",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/yuannian1112/jd_scripts/main/jd_jxmc.js",
+	"name": "京喜牧场"
+},{
+	"id": "jd_lsj",
+	"isConcurrent":false,
+	"cron": "0 0 11 * * * *",
+	"describe":"入口 京东 频道 美食馆",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_lsj.js",
+	"name": "零食街"
+},{
+	"id": "gua_MMdou",
+	"isConcurrent":false,
+	"cron": "0 10 0 * * * *",
+	"describe":"入口：首页-领京豆-升级赚京豆",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/gua_MMdou.js",
+	"name": "升级领京豆"
+},{
+	"id": "jd_mohe",
+	"isConcurrent":false,
+	"cron": "0 20 0,1-23/3 * * * *",
+	"describe":"5G超级盲盒，可抽奖获得京豆，建议在凌晨0点时运行脚本，白天抽奖基本没有京豆，4小时运行一次收集热力值",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_mohe.js",
+	"name": "5G盲盒"
+},{
+	"id": "jd_cash",
+	"cron": "0 32 0,1,2 * * * *",
+	"describe":"活动入口：京东APP搜索领现金进入",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_cash.js",
+	"name": "签到领现金"
+},{
+	"id": "jd_bean_change_new",
+	"cron": "0 30 10,22 * * * *",
+	"describe":"京东资产变动通知",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_bean_change_new.js",
+	"name": "京东资产变动通知"
+},{
+	"id": "jd_dwapp",
+	"cron": "0 33 5,17 * * * *",
+	"describe":"首页-生活·缴费-积分换话费",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_dwapp.js",
+	"name": "积分换话费"
+},{
+    "id": "jd_joypark_joy",
+	"cron": "0 20 0-23/3 * * * *",
+	"describe":"汪汪乐园养joy",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_joy_park_task.js",
+	"name": "汪汪乐园养joy"
+},{
+    "id": "jd_connoisseur",
+	"cron": "0 15 3,6 * * * *",
+	"describe":"内容鉴赏官",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_connoisseur.js",
+	"name": "内容鉴赏官"
+},{
+    "id": "jd_wish",
+	"cron": "0 40 0,2 * * * *",
+	"describe":"活动入口：京东-京东众筹-众筹许愿池",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/yuannian1112/jd_scripts/main/jd_wish.js",
+	"name": "众筹许愿池"
+},{
+    "id": "jd_ddworld",
+	"cron": "0 0 0 * * * *",
+	"describe":"东东世界",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_ddworld.js",
+	"name": "东东世界"
+},{
+    "id": "jd_ddwj",
+	"cron": "0 20 0 * * * *",
+	"describe":"东东玩家",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_ddwj.js",
+	"name": "东东玩家"
+},{
+    "id": "jd_xmf",
+	"cron": "0 6 8,20 * * * *",
+	"describe":"集魔方",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/zero205/JD_tencent_scf/main/jd_jmf.js",
+	"name": "集魔方"
+},{
+    "id": "jd_jmsign",
+	"cron": "0 11 0,9 * * * *",
+	"describe":"芥么签到",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_jmsign.js",
+	"name": "芥么签到"
+},{
+    "id": "jd_car_exchange",
+	"cron": "0 0 0 * * * *",
+	"describe":"活动入口：京东APP首页-京东汽车-屏幕右中部，车主福利",
+	"url": "https://ghproxy.com/https://github.com/zero205/JD_tencent_scf/blob/main/jd_car_exchange.js",
+	"name": "京东汽车兑换"
+},{
+    "id": "jd_car",
+	"cron": "0 10 7 * * * *",
+	"describe":"活动入口：京东APP首页-京东汽车-屏幕右中部，车主福利",
+	"url": "https://ghproxy.com/https://github.com/zero205/JD_tencent_scf/blob/main/jd_car.js",
+	"name": "京东汽车"
+},{
+	"id": "jd_speed_signfree",
+	"cron": "0 18 8,12,20 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_speed_signfree.js",
+	"name": "签到免单"
+},{
+	"id": "jd_superbox",
+	"isConcurrent":false,
+	"cron": "0 24 2,3 * * * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/small-redguy/helper/main/jdresource/jd_superbox.js",
+	"name": "京东超级盒子"
+},{
+	"id": "gua_wealth_island",
+	"isConcurrent":false,
+	"cron": "0 10 9,13,16,19,20 2-8 11 * *",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/faker2/main/gua_wealth_island.js",
+	"name": "财富大陆"
+},{
+	"id": "gua_wealth_island",
+	"isConcurrent":false,
+	"cron": "",
+	"url": "https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_get_share_code.js",
+	"name": "互助码"
+}]
